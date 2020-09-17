99f79e0160f6ce62f699157fff175981bfbbccdb
Ian Yin <yin.ian093@gmail.com>
2020-09-18 00:28:16 +1000

    add orders' router

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -5,5 +5,15 @@ const {
     getOrder,
     getAllOrder,
     completeOrder,
-    
-}
+    deleteOrder,
+} = require('../controllers/orders');
+
+const router = express.Router();
+
+router.get('/', getAllOrder);
+router.get('/:id', getOrder);
+router.post('/', addOrder);
+router.put('/:id/complete', completeOrder);
+router.put('/:id/delete', deleteOrder);
+
+module.exports = router;
