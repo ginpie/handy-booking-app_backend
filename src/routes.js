8b75f0722bc55de9afb36da3614367a9c2e36222
chen <chenjp1989@gmail.com>
2020-09-19 18:15:09 +1000

    add inquiry routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const orderRouter = require('./routes/orders');
+const orderRouter = require("./routes/orders");
+const inquiryRoute = require("./routes/inquiries");
 
-router.use('/order', orderRouter);
-
+router.use("/order", orderRouter);
+router.use("/inquiry", inquiryRoute);
 
 module.exports = router;
