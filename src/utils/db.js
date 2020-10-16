23111f0f61935aad21d5ed78dc179415a71d22ab
chen <chenjp1989@gmail.com>
2020-10-16 12:49:19 +1100

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,25 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 exports.connectToDB = () => {
-  const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  const {
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USER,
+    DB_PASSWORD,
+    NODE_ENV,
+  } = process.env;
+
+  let connectionString;
+  if (NODE_ENV === "production") {
+    connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`;
+  } else {
+    connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  }
+
   // const connectionString = 'mongodb://localhost:27017/handyman-test';
 
   const db = mongoose.connection;
-  db.on('connected', () => {
-    console.log('DB connected');
+  db.on("connected", () => {
+    console.log(`DB connected, ${connectionString}`);
   });
-  db.on('error', (error) => {
-    console.log('DB connection failed');
+  db.on("error", (error) => {
+    console.log("DB connection failed");
     console.error(error.message);
     process.exit(1);
   });
-  db.on('disconnected', () => {
-    console.log('mongoose connection is disconnected');
+  db.on("disconnected", () => {
+    console.log("mongoose connection is disconnected");
   });
 
   mongoose.connect(connectionString, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
 };
