0aaefe162a9592d2106a6f56c9d098175f8662f8
Hunterprey <595129026@qq.com>
2020-10-18 14:36:39 +1000

    tradie & customer

--- src/routes/tradies.js
+++ src/routes/tradies.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const {
   getAllTradies,
-  getTradie,
+  getTradieAllInfo,
   addTradie,
   deleteTradie,
   updateTradie,
   addJobForTradie,
   addOrderForTradie,
+  getTradieInquiries,
 } = require("../controllers/tradies");
 const router = express.Router();
 
 router.get("/", getAllTradies);
-router.get("/:id", getTradie);
+router.get("/:id", getTradieAllInfo);
+router.get("/:id/inquiry", getTradieInquiries);
 router.post("/", addTradie);
 router.put("/:id", updateTradie);
 router.delete("/:id", deleteTradie);
