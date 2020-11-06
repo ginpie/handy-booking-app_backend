158dcef2e373de3394beb7a0952e81b14b063750
Hunterprey <595129026@qq.com>
2020-11-07 00:42:50 +1000

    customerGet Inquiry

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -9,6 +9,7 @@ const {
   getCustomerOrderInfo,
   CustomersSendInquiry,
   addInquiryForCustomer,
+  getCustomerInquiryInfo,
 } = require("../controllers/customers");
 const router = express.Router();
 
@@ -19,6 +20,7 @@ router.put("/:id", updateCustomerAddress);
 router.delete("/:id", deleteCustomer);
 router.post("/:id/order/:code", addOrderForCustomers);
 router.get("/:id/order", getCustomerOrderInfo);
+router.get("/:id/inquiry", getCustomerInquiryInfo);
 router.post("/:id/inquiry/:code", CustomersSendInquiry);
 router.put("/:id/inquiry", addInquiryForCustomer);
 
